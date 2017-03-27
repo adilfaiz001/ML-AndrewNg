b0d633f90fde6e225a0f6990cc9402616b2c5b3f
Adil Faiz <adilfaiz01@live.com>
2017-03-27 17:18:19 +0530

    Multivariate Regression Completed

--- Solution/machine-learning-ex1/ex1/ex1_multi.m
+++ Solution/machine-learning-ex1/ex1/ex1_multi.m
@@ -105,7 +105,10 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
-
+normalizedSquareFeet = (1650 - mu) / sigma;
+normalizedNumberOfBedrooms = (3 - mu) / sigma;
+i=[1, normalizedSquareFeet, normalizedNumberOfBedrooms];
+price=i*theta;
 
 % ============================================================
 
@@ -150,7 +153,7 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+price = [1, 1650, 3] * theta; 
 
 % ============================================================
 
