941e8bf0ad56914c687e4db64fc35b50bf8a8162
Adil Faiz <adilfaiz01@live.com>
2017-03-27 16:27:48 +0530

    Gradient Descent

--- Solution/machine-learning-ex1/ex1/gradientDescent.m
+++ Solution/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,13 +16,18 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+     % Calculating the transpose of our hypothesis
 
-    
+    h = (X * theta - y)';        
 
+    % Updating the parameters
 
+    theta(1) = theta(1) - alpha * (1/m) * h * X(:, 1);
 
+    theta(2) = theta(2) - alpha * (1/m) * h * X(:, 2);        
 
-
+    % Keeping track of the cost function
+       
     % ============================================================
 
     % Save the cost J in every iteration    
